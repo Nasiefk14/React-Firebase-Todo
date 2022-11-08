b6f6a76af6fa02f2443321ae262308cee57eae8b
Nasief Khan <nasiefk14@gmail.com>
2022-11-08 15:01:00 +0200

    Added Toastify To Add Todo Button

--- todo-app/src/components/todos/AddTodo.jsx
+++ todo-app/src/components/todos/AddTodo.jsx
@@ -3,7 +3,8 @@ import { db } from '../../firebase';
 import { collection, addDoc, serverTimestamp} from 'firebase/firestore';
 import { useState } from 'react';
 import './AddTodo.css'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from 'react-redux';
 import { selectUserId, selectUserName } from '../../features/user/userSlice';
 
@@ -11,10 +12,24 @@ const AddTodo = () => {
     const [title, setTitle] = useState('')
     const dbId = useSelector(selectUserId)
     const userName = useSelector(selectUserName)
+    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (title !== '') {
+        if (title.match(/^\s*$/)) {
+            toast.error('Enter A Valid Value!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                fontFamily: "Century Gothic"
+                });
+
+        } else {
             await addDoc(collection(db, `${dbId} - ${userName}`), {
                 title,
                 completed: false,
@@ -25,6 +40,7 @@ const AddTodo = () => {
     }
     return (
         <form onSubmit={handleSubmit}>
+            <ToastContainer />
             <div className='inputContainer'>
                 <input type="text" placeholder='Add Todo...' value={title} onChange={(e) => setTitle(e.target.value)}/>
             </div>
